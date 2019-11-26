30a89fabbfa299a56f6b8941a6d1e8a1ee4e001e
LaustinSpayce <stuart.myers@gmail.com>
2019-11-26 16:48:16 +0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,22 @@ console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
